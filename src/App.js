106862f0b24c03767be9f496cb466731d1341f7a
walkwel <developer.up90@gmail.com>
2018-03-16 20:07:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,21 +3,21 @@ import { Link } from "react-router-dom";
 const App = () => (
   <div>
     <ul>
-      <Link to = '/example1' target = '_blank'>
+      <Link to = '/example1'>
         <li>
           <p>Example1 </p>
         </li>
       </Link>
-      <Link to = "/example2" target = '_blank'>
+      <Link to = "/example2" >
         <li>
           <p>Example2 </p>
         </li>
       </Link>
-      <Link to = "/example3" target = '_blank'>
-        <li>
-          <p>Example3 </p>
-        </li>
-      </Link>
+//       <Link to = "/example3" target = '_blank'>
+//         <li>
+//           <p>Example3 </p>
+//         </li>
+//       </Link>
       
     </ul>
   </div>
